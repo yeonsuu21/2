1bbdcc46b1df960d4d9ecd622ca00b2ed9d77442
yeonssu21 <kimys9295@naver.com>
2024-06-24 22:04:12 +0900

    풍차 만들기

--- 2-2/src/App.js
+++ 2-2/src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [n, setN] = useState(0);
+  const [pattern, setPattern] = useState('');
+
+  const handleClick = () => {
+    const newN = n + 1;
+    setN(newN);
+    generatePattern(newN);
+  };
+
+  const generatePattern = (newN) => {
+    let result = '';
+    // 상, 하로 나누어서 for문 작성
+    for (let i = 1; i <= newN; i++) {
+      // '⭐️' 이 기준으로 처음부터 
+      for (let j = 0; j < i; j++) {
+        result += ' ';
+      }
+      for (let j = newN - i; j >= 0; j--) {
+        result += 'r';
+      }
+      for (let j = newN - i; j > 0; j--) {
+        result += ' ';
+      }
+      for (let j = i-1; j >= 0; j--) {
+        result += 'r';
+      }
+      result += '\n';
+    }
+
+    for (let i = 1; i <= newN; i++) {
+      for (let j = newN-i; j >= 0; j--) {
+        result += 'r';
+      }
+      for (let j = i; j > 0; j--) {
+        result += ' ';
+      }
+      for (let j = i; j > 0; j--) {
+        result += 'r';
+      }
+      for (let j = newN - i; j > 0; j--) {
+        result += ' ';
+      }
+      result += '\n';
+    }
+
+    setPattern(result);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>풍차</h1>
+        <div>
+          <button onClick={handleClick}>{n}+</button>
+        </div>
+        <pre>{pattern}</pre>
       </header>
     </div>
   );
